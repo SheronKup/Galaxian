eefa4553f294fa018f5cffb65b3c04fe46d98351
Enzo Molinari Reis <enzoreis.tj@gmail.com>
2024-03-20 09:25:09 -0300

    Feita animação em processo de colisão de tiros

--- main.js
+++ main.js
@@ -2,58 +2,50 @@ let des = document.getElementById('des').getContext('2d')
 
 let nav1 = new Nave(400,500,28,29,'./assets/nave-1.png')
 
-let enemy1 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-//Alterado Aqui
+let enemy1 = new Disco1(300, 100, 30, 30,'./assets/0.png')
+// let enemy2 = new Disco1(20, 10, 30, 30,'./assets/bixo.png')
+// let enemy3 = new Disco1(60, 10, 30, 30,'./assets/bixo.png')
+// let enemy4 = new Disco1(100, 10, 30, 30,'./assets/bixo.png')
+// let enemy5 = new Disco1(140, 10, 30, 30,'./assets/bixo.png')
+// let enemy6 = new Disco1(180, 10, 30, 30,'./assets/bixo.png')
+// let enemy7 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy8 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy9 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy10 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy11 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy12 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy13 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy14 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy15 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy16 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy17 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy18 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy19 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
+// let enemy20 = new Disco1(10, 10, 50, 50,'./assets/bixo.png')
 
-// let enemy2 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy3 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy4 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy5 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy6 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy7 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy8 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy9 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy10 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy11 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy12 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy13 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy14 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy15 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy16 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy17 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy18 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy19 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-// let enemy20 = new Disco1(10, 10, 50, 50,'./assers/bixo.png')
-let alien = new Disco2(125,156,22,22,'sdffda')
-let lien1 = new Disco2(125,156,22,22,'sdffda')
-let lien2= new Disco2(125,156,22,22,'sdffda')
-let lien3 = new Disco2(125,156,22,22,'sdffda')
-let lien4 = new Disco2(125,156,22,22,'sdffda')
-let lien5 = new Disco2(125,156,22,22,'sdffda')
-let lien6 = new Disco2(125,156,22,22,'sdffda')
-let lien7 = new Disco2(125,156,22,22,'sdffda')
-let lien8 = new Disco2(125,156,22,22,'sdffda')
-let lien9 = new Disco2(125,156,22,22,'sdffda')
-let lien10 = new Disco2(125,156,22,22,'sdffda')
+// let alien1 = new Disco2(110, 10, 50, 50,'./assets/bixo.png')
+// let alien2 = new Disco2(10, 10, 50, 50,'./assets/bixo.png')
+// let alien3 = new Disco2(10, 10, 50, 50,'./assets/bixo.png')
+// let alien4 = new Disco2(10, 10, 50, 50,'./assets/bixo.png')
+// let alien5 = new Disco2(10, 10, 50, 50,'./assets/bixo.png')
+// let alien6 = new Disco2(10, 10, 50, 50,'./assets/bixo.png')
+// let alien7 = new Disco2(10, 10, 50, 50,'./assets/bixo.png')
+// let alien8 = new Disco2(10, 10, 50, 50,'./assets/bixo.png')
 
+// let hex1 = new Disco3(10, 10, 50, 50,'./assets/bixo.png')
+// let hex2 = new Disco3(10, 10, 50, 50,'./assets/bixo.png')
+// let hex3 = new Disco3(10, 10, 50, 50,'./assets/bixo.png')
+// let hex4 = new Disco3(10, 10, 50, 50,'./assets/bixo.png')
+// let hex5 = new Disco3(10, 10, 50, 50,'./assets/bixo.png')
+// let hex6 = new Disco3(10, 10, 50, 50,'./assets/bixo.png')
 
-let alien7 = new Disco2(10, 10, 50, 50,'./assers/bixo.png')
-let alien8 = new Disco2(10, 10, 50, 50,'./assers/bixo.png')
+// let quadra1 = new Disco4(10, 10, 50, 50,'./assets/bixo.png')
+// let quadra2 = new Disco4(10, 10, 50, 50,'./assets/bixo.png')
+// let quadra3 = new Disco4(10, 10, 50, 50,'./assets/bixo.png')
+// let quadra4 = new Disco4(10, 10, 50, 50,'./assets/bixo.png')
 
-let hex1 = new Disco3(10, 10, 50, 50,'./assers/bixo.png')
-let hex2 = new Disco3(10, 10, 50, 50,'./assers/bixo.png')
-let hex3 = new Disco3(10, 10, 50, 50,'./assers/bixo.png')
-let hex4 = new Disco3(10, 10, 50, 50,'./assers/bixo.png')
-let hex5 = new Disco3(10, 10, 50, 50,'./assers/bixo.png')
-let hex6 = new Disco3(10, 10, 50, 50,'./assers/bixo.png')
-
-let quadra1 = new Disco4(10, 10, 50, 50,'./assers/bixo.png')
-let quadra2 = new Disco4(10, 10, 50, 50,'./assers/bixo.png')
-let quadra3 = new Disco4(10, 10, 50, 50,'./assers/bixo.png')
-let quadra4 = new Disco4(10, 10, 50, 50,'./assers/bixo.png')
-
-let duo1 = new Disco5(10, 10, 50, 50,'./assers/bixo.png')
-let duo2 = new Disco5(10, 10, 50, 50,'./assers/bixo.png')
+// let duo1 = new Disco5(10, 10, 50, 50,'./assets/bixo.png')
+// let duo2 = new Disco5(10, 10, 50, 50,'./assets/bixo.png')
 
 let txt_pts = new Texto()
 let pts = new Texto()
@@ -65,18 +57,7 @@ som1.volume = 1.0
 som1.loop = true
 som2.volume = 0.7
 
-let enemyLines = [
-    // Line 1 (adjust x and y as needed)
-    [{ x: 100, y: 100 }, { x: 200, y: 100 }, { x: 300, y: 100 }],
-    // Line 2 (adjust x and y as needed)
-    [{ x: 50, y: 200 }, { x: 150, y: 200 }, { x: 250, y: 200 }],
-    // Line 3 (adjust x and y as needed)
-    [{ x: 50, y: 200 }, { x: 150, y: 200 }, { x: 250, y: 200 }],
-    // Line 4 (adjust x and y as needed)
-    [{ x: 50, y: 200 }, { x: 150, y: 200 }, { x: 250, y: 200 }],
-    // Line 5 (adjust x and y as needed)
-    [{ x: 50, y: 200 }, { x: 150, y: 200 }, { x: 250, y: 200 }],
-  ];
+
 
 let grupoTiros = [] 
 let tiros = {
@@ -95,6 +76,20 @@ let tiros = {
     }
 }
 
+const tiro = document.getElementById('tiros');
+const inimigo = document.getElementById('enemy1');
+
+function detectarColisao() {
+  const posicaoTiro = tiro.getBoundingClientRect();
+  const posicaoInimigo = inimigo.getBoundingClientRect();
+
+  if (posicaoTiro.left < posicaoInimigo.right &&
+    posicaoTiro.right > posicaoInimigo.left &&
+    posicaoTiro.top < posicaoInimigo.bottom &&
+    posicaoTiro.bottom > posicaoInimigo.top) {
+    inimigo.parentNode.removeChild(inimigo);
+  }
+}
 
 document.addEventListener('keydown', (ev)=>{
     if(ev.key === 'a'){
@@ -130,43 +125,46 @@ document.addEventListener('keypress', (ev) => {
 });
 
 let grupoDiscos = []
+grupoDiscos.push(enemy1)
 let discos = {
     time1: 0, 
     time2: 0,
     time3: 0,
     time4: 0,
 
-    criaDisco(){
-        this.time1 += 1
-        this.time2 += 1
-        this.time3 += 1
-        this.time4 += 1
-        let pos_x = (Math.random() * (438 - 2 +1)+2)
-        let pos_x2 = (Math.random() * (438 - 2 +1)+2)
-        let pos_x3 = (Math.random() * (438 - 2 +1)+2)
-        let pos_x4 = (Math.random() * (438 - 2 +1)+2)
-        if(this.time1 >=60){
-            this.time1 = 0
-            grupoDiscos.push(new Disco(pos_x,-200,50,50,'assets/disco.png'))
-            console.log(grupoDiscos)
-        }
-        if(this.time2 >=85){
-            this.time2 = 0
-            grupoDiscos.push(new Disco(pos_x2,-300,50,50,'assets/disco2.png'))
-            console.log(grupoDiscos)
-        }
+    
 
-        if(this.time3 >=135){
-            this.time3 = 0
-            grupoDiscos.push(new Disco(pos_x3,-400,50,50,'assets/disco3.png'))
-            console.log(grupoDiscos)
-        }
-        if(this.time4 >=135){
-            this.time = 0
-            grupoDiscos.push(new Disco(pos_x3,-400,50,50,'assets/disco3.png'))
-            console.log(grupoDiscos)
-        }
-    },
+    // criaDisco(){
+    //     this.time1 += 1
+    //     this.time2 += 1
+    //     this.time3 += 1
+    //     this.time4 += 1
+    //     let pos_x = (Math.random() * (438 - 2 +1)+2)
+    //     let pos_x2 = (Math.random() * (438 - 2 +1)+2)
+    //     let pos_x3 = (Math.random() * (438 - 2 +1)+2)
+    //     let pos_x4 = (Math.random() * (438 - 2 +1)+2)
+    //     if(this.time1 >=60){
+    //         this.time1 = 0
+    //         grupoDiscos.push(new Disco(pos_x,-200,50,50,'assets/disco.png'))
+    //         console.log(grupoDiscos)
+    //     }
+    //     if(this.time2 >=85){
+    //         this.time2 = 0
+    //         grupoDiscos.push(new Disco(pos_x2,-300,50,50,'assets/disco2.png'))
+    //         console.log(grupoDiscos)
+    //     }
+
+    //     if(this.time3 >=135){
+    //         this.time3 = 0
+    //         grupoDiscos.push(new Disco(pos_x3,-400,50,50,'assets/disco3.png'))
+    //         console.log(grupoDiscos)
+    //     }
+    //     if(this.time4 >=135){
+    //         this.time = 0
+    //         grupoDiscos.push(new Disco(pos_x3,-400,50,50,'assets/disco3.png'))
+    //         console.log(grupoDiscos)
+    //     }
+    // },
     des(){
         grupoDiscos.forEach((disc)=>{
             disc.des_obj()
@@ -179,7 +177,6 @@ let discos = {
                     grupoTiros.splice(grupoTiros.indexOf(tiro), 1)
                     grupoDiscos.splice(grupoDiscos.indexOf(disc), 1)
                     nav1.pts +=1
-                    podeAtirar = true
                 }
             })
         })
@@ -206,9 +203,15 @@ function colisao(){
 }
 
 function desenha(){    
-  tiros.des();
-  discos.des();
-  nav1.des_obj();  
+    tiros.des();
+    discos.des();
+    enemy1.des_obj();
+//  enemy2.des_obj();
+//  enemy3.des_obj();
+//  enemy4.des_obj();
+//  enemy5.des_obj();
+//  enemy6.des_obj();
+    nav1.des_obj();
 
   txt_pts.des_text('Pontos:',245,40,'white','26px Times');
   pts.des_text(nav1.pts,277,70,'white','26px Times');
@@ -218,9 +221,11 @@ function desenha(){
 
 function atualiza(){
     tiros.atual()
-    // discos.atual()
-    nav1.mov()
-    colisao()    
+    enemy1.mov();
+    nav1.mov();
+    nav1.anim('nave-');
+    colisao();
+    // detectarColisao();
 }
 
 function main(){
